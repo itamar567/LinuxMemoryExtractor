d6013c53d96ee0317351cd4bf7584101ab848814
itamar567 <itamar.shabtai.1@gmail.com>
2023-02-01 21:12:22 +0200

    Initial commit

---
+++ client.py
@@ -0,0 +1,58 @@
+import argparse
+import os
+import socket
+import math
+
+
+# Configurable
+# Must be the same values as the server
+MAX_BYTES_FOR_FILE_SIZE = 2
+
+# Auto-generated
+MAX_FILE_SIZE = ((2 ** 8) ** MAX_BYTES_FOR_FILE_SIZE) - 1
+
+
+def get_args():
+    """
+    Get the program arguments
+    """
+    parser = argparse.ArgumentParser(prog='LinuxMemoryExtractor Client')
+    parser.add_argument('host')
+    parser.add_argument('port')
+    parser.add_argument('filename')
+    return parser.parse_args()
+
+
+args = get_args()
+
+file_path = os.path.abspath(args.filename)
+
+file = open(file_path, "rb")
+file_data = file.read()
+file.close()
+
+file_length = len(file_data)
+
+s = socket.socket()
+s.connect((args.host, int(args.port)))
+
+number_of_packets = math.ceil(file_length / MAX_FILE_SIZE)
+
+for i in range(number_of_packets):
+    packet_data = file_data[i * MAX_FILE_SIZE:(i+1) * MAX_FILE_SIZE]
+    packet_data_length = min(MAX_FILE_SIZE, file_length - (i * MAX_FILE_SIZE))
+    last_packet = (i == number_of_packets - 1)
+    if last_packet:
+        last_packet_flag = int(1).to_bytes(1, "little")
+    else:
+        last_packet_flag = int(0).to_bytes(1, "little")
+
+    packet_message = \
+        packet_data_length.to_bytes(MAX_BYTES_FOR_FILE_SIZE, "little") \
+        + last_packet_flag + packet_data
+
+    print(f"Sending packet {i+1}/{number_of_packets}"
+          + f" (Length = {packet_data_length})")
+    s.sendall(packet_message)
+
+print("All packets sent successfully")
---
+++ server.c
@@ -0,0 +1,111 @@
+#define _GNU_SOURCE
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/socket.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <sys/mman.h>
+#include <fcntl.h>
+#include <endian.h>
+
+#define MAX_PENDING_CONN_QUEUE_LENGTH 10
+#define MAX_BYTES_FOR_FILE_SIZE 2
+#define MAX_BYTES_FOR_FILE_LENGTH 1024
+
+int main(int argc, char const* argv[]){
+  // Handle command-line args
+  if (argc < 2) {
+    printf("LinuxMemoryExtractor Client\n");
+    printf("Usage: %s <port>\n", argv[0]);
+    printf("\nERROR: port not specified\n");
+    exit(EXIT_FAILURE);
+  }
+
+  struct sockaddr_in address;
+  address.sin_family = AF_INET;
+  address.sin_addr.s_addr = INADDR_ANY;
+  address.sin_port = htons(atoi(argv[1]));
+
+  socklen_t address_size = sizeof(address);
+
+  int socket_fd = socket(AF_INET, SOCK_STREAM, 0);
+  if (socket_fd < 0) {
+    perror("socket");
+    exit(EXIT_FAILURE);
+  }
+
+  if (bind(socket_fd, (struct sockaddr*) &address, address_size) < 0) {
+    perror("bind");
+    exit(EXIT_FAILURE);
+  }
+
+  if (listen(socket_fd, MAX_PENDING_CONN_QUEUE_LENGTH) < 0) {
+    perror("listen");
+    exit(EXIT_FAILURE);
+  }
+
+  printf("Server started\n");
+
+  while (1){
+    int connected_socket = accept(socket_fd, (struct sockaddr*) &address, &address_size);
+    if (connected_socket < 0) {
+      perror("accept");
+      continue;
+    }
+
+    printf("Accepted new connection\n");
+
+    int data_fd = memfd_create("client_data", 0);
+    if (data_fd < 0) {
+      perror("memfd_create");
+      continue;
+    }
+
+    int finished = 0;
+    while (!finished){
+      unsigned char header_buffer[MAX_BYTES_FOR_FILE_SIZE + 1];
+      if (read(connected_socket, header_buffer, MAX_BYTES_FOR_FILE_SIZE + 1) < 0) { // +1 for the 'last packet' flag
+        perror("read");
+        continue;
+      }
+
+      int data_size = 0;
+      for (int i = 0;i < MAX_BYTES_FOR_FILE_SIZE;i++) {
+        data_size += header_buffer[i] << (8 * i);
+      }
+
+      int last_packet = header_buffer[MAX_BYTES_FOR_FILE_SIZE];
+
+      printf("Receiving packet (Length = %d, Last = %d)\n", data_size, last_packet);
+
+      void* data_buffer[data_size];
+
+      if (read(connected_socket, data_buffer, data_size) < 0) { // +1 for the 'last packet' flag
+        perror("read");
+        continue;
+      }
+
+      if (write(data_fd, data_buffer, data_size) < 0) {
+        perror("write");
+        continue;
+      }
+
+      if (last_packet) {
+        finished = 1;
+      }
+    }
+    
+    if (!finished) { // An error occured
+      continue;
+    }
+
+    printf("Received all packets, executing program\n");
+
+    const char * const data_argv[] = {NULL};
+    const char * const data_envp[] = {NULL};
+
+    if (fork() == 0) {
+      fexecve(data_fd, (char * const *) data_argv, (char * const *) data_envp);
+    }
+  }
+}
